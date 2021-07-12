00eb4d0a5bd239cd2981aeec8561a0fd8f63963e
adel037 <43687705+adel037@users.noreply.github.com>
2021-07-12 14:46:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Adrian_personal_website
---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Cherry-Blossom-Wallpaper.jpg') {
+      myImage.setAttribute ('src','images/x6JeNye.jpg');
+    } else {
+      myImage.setAttribute ('src','images/Cherry-Blossom-Wallpaper.jpg');
+    }
+}
+
+//--- user welcome 
+let myButton = document.querySelector('button');
+myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} 
+else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome, ' + storedName;
+}
+
+//let user change username
+myButton.onclick = function() {
+    setUserName();
+  }
+//---
